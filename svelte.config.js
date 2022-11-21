6493cd988a6bc164b56b09a4dc548b2179cc424e
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2022-11-21 13:09:07 +0100

    feat: use adapter vercel for edge deployments

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
-};
+  kit: {
+    adapter: vercel( {
+      edge: true,
+    } )
+  }
+}
 
-export default config;
+export default config
