f35b11614acb67ec0e79fdb03b54443b3c4124b2
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2022-11-23 03:23:49 +0100

    Update +page.svelte

--- src/routes/[source]/[content]/+page.svelte
+++ src/routes/[source]/[content]/+page.svelte
@@ -7,7 +7,7 @@
   import Theme from "$lib/Theme.svelte";
   import Article from "./Article.svelte";
   import Publication from "./Publication.svelte";
-    import ActionButton from "$lib/ActionButton.svelte";
+  import ActionButton from "$lib/ActionButton.svelte";
 
   export let data: PageData;
   const config: Config = data.config as any;
@@ -30,7 +30,7 @@
   <meta property="og:site_name" content={config.siteName} />
   <meta property="og:title" content={config.title} />
   <meta property="og:description" content={config.description} />
-  <meta property="og:image" content={config.image} />
+  <meta property="og:image" content={config.imageUrl || config.image} />
   <meta property="og:image:alt" content={config.imageAlt} />
   <meta property="og:type" content="article" />
   <meta property="og:article:author" content={config.author} />
@@ -38,7 +38,7 @@
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content={config.title} />
   <meta name="twitter:description" content={config.description} />
-  <meta name="twitter:image" content={config.image} />
+  <meta name="twitter:image" content={config.imageUrl || config.image} />
   <meta name="twitter:image:alt" content={config.imageAlt} />
 </svelte:head>
 
