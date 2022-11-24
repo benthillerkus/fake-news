1b19dc86c8435da0eb9a05286489452919eed86c
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2022-11-25 00:29:10 +0100

    feat: support markdown??

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
   import type { Config } from "$lib/types";
+  import SvelteMarkdown from "svelte-markdown";
   import "$lib/styles/reset.css";
   import "$lib/styles/systemfonts.css";
   import Time from "$lib/components/Time.svelte";
 
+  const lorem = `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+ipsum dolor sit amet.`;
+
   export let config: Config;
+
+  $: source = config.content != "" ? config.content : lorem;
 </script>
 
 <article id="article" itemscope itemtype="https://schema.org/BlogPosting">
@@ -15,26 +28,12 @@
     </h1>
     <p itemprop="abstract">{config.description}</p>
     <span
-    >Published <Time itemprop="datePublished" time={config.publishedDate} />
-    by <b itemprop="author">{config.author}</b></span
+      >Published <Time itemprop="datePublished" time={config.publishedDate} />
+      by <b itemprop="author">{config.author}</b></span
     >
-    <hr>
+    <hr />
   </header>
   <main itemprop="articleBody">
-    {#if config.content}
-      <p>{config.content}</p>
-    {:else}
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
-      </p>
-    {/if}
+    <SvelteMarkdown {source} />
   </main>
 </article>
