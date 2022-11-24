9662b02055c93c9189cb0b1659dd5517ebb82ba9
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2022-11-24 04:18:10 +0100

    fix: remove enable postcss line

--- svelte.config.js
+++ svelte.config.js
@@ -5,13 +5,10 @@ import preprocess from 'svelte-preprocess'
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess( {
-    postcss: true,
-  } ),
-
+  preprocess: preprocess(),
   kit: {
     adapter: vercel( {
-      edge: true,
+      edge: true
     } )
   }
 }
