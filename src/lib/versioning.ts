1104bc83f99c5e2bee96c0a39bd18c52b73ac95c
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2022-11-24 04:36:36 +0100

    fix: include renames in versioning

--- src/lib/versioning.ts
+++ src/lib/versioning.ts
@@ -2,18 +2,26 @@ import type { Config } from "$lib/types";
 
 export function upgradeVersion(config: Config) {
 
-  const rename = (oldName: string, newName: string) => {
+  const renameKey = (oldName: string, newName: string) => {
     if (oldName in config) {
       config[newName] = config[oldName];
       delete config[oldName];
     }
   }
 
+  const renameValue = (key: string, oldName: string, newName: string) => {
+    if (config[key] === oldName) {
+      config[key] = newName;
+    }
+  }
+
   switch (config.version) {
     case undefined:
-      rename("image", "imageUrl")
+      renameKey("image", "imageUrl")
+      renameValue("style", "blog", "Blog")
+      renameValue("style", "newspaper", "News")
     case "1":
-      rename("favicon", "icon")
+      renameKey("favicon", "icon")
     case "2":
   }
   config.version = "2";
